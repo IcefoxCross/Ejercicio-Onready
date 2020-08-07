8e8d28375f7733fcf77a43dd84d9e5e540d1dd62
Cesar <reuscesar9@gmail.com>
2020-08-07 13:59:10 -0300

    Update - Corregido para utilizar OOP

--- bin/index.js
+++ bin/index.js
@@ -1,51 +1,61 @@
 #!/usr/bin/env node
 
+// Clases
+class Vehiculo {
+    constructor(marca, modelo, precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    precio_formateado() {
+        return this.precio.toLocaleString('en-EN', {style: "currency", currency: "USD", minimumFractionDigits: 2});
+    }
+
+    informacion() {
+        console.log(`Marca: ${this.marca} // Modelo: ${this.modelo} // Precio: ${this.precio_formateado()}`);
+    }
+}
+
+class Auto extends Vehiculo {
+    constructor(marca, modelo, puertas, precio) {
+        super(marca, modelo, precio);
+
+        this.puertas = puertas;
+    }
+
+    informacion() {
+        console.log(`Marca: ${this.marca} // Modelo: ${this.modelo} // Puertas: ${this.puertas} // Precio: ${this.precio_formateado()}`);
+    }
+}
+
+class Moto extends Vehiculo {
+    constructor(marca, modelo, cilindrada, precio) {
+        super(marca, modelo, precio);
+
+        this.cilindrada = cilindrada;
+    }
+
+    informacion() {
+        console.log(`Marca: ${this.marca} // Modelo: ${this.modelo} // Cilindrada: ${this.cilindrada} // Precio: ${this.precio_formateado()}`);
+    }
+}
+
 const datos = [];
 
 
 // Carga listado de vehiculos a procesar
 const cargarDatos = () => {
-    datos.push({
-        Marca: "Peugeot",
-        Modelo: "206",
-        Puertas: 4,
-        Precio: 200000.00
-    });
-    datos.push({
-        Marca: "Honda",
-        Modelo: "Titan",
-        Cilindrada: "125c",
-        Precio: 60000.00
-    });
-    datos.push({
-        Marca: "Peugeot",
-        Modelo: "208",
-        Puertas: 5,
-        Precio: 250000.00
-    });
-    datos.push({
-        Marca: "Yamaha",
-        Modelo: "YBR",
-        Cilindrada: "160c",
-        Precio: 80000.50
-    });
+    datos.push(new Auto("Peugeot", "206", 4, 200000.00));
+    datos.push(new Moto("Yamaha", "Titan", "125c", 60000.00));
+    datos.push(new Auto("Peugeot", "208", 5, 250000.00));
+    datos.push(new Moto("Yamaha", "YBG", "160c", 80000.50));
 };
 
 // Imprime listado total de vehiculos con toda su informacion
 imprimirListado = () => {
     datos.forEach(vehiculo => {
-        let salida = "";
-        for (const key in vehiculo) {
-            formato = `${key}: `;
-            if (key !== "Precio") {
-                formato += `${vehiculo[key]} // `;
-            } else {
-                // Si el campo es el precio, modifica a un estilo de moneda
-                formato += `${vehiculo[key].toLocaleString('en-EN', {style: "currency", currency: "USD", minimumFractionDigits: 2})}`;
-            }
-            salida += formato;
-        }
-        console.log(salida);
+        vehiculo.informacion();
     });
 };
 
@@ -53,24 +63,24 @@ imprimirEstadisticas = () => {
     // Crea copia del listado de vehiculos para modificar
     const copia = [...datos];
     // Ordena la lista copiada de mayor a menor precio
-    copia.sort((a,b) => b.Precio - a.Precio);
-    const encontrado = copia.find(vehiculo => vehiculo.Modelo.includes("Y"));
+    copia.sort((a,b) => b.precio - a.precio);
+    const encontrado = copia.find(vehiculo => vehiculo.modelo.includes("Y"));
     // Extrae el primero y ultimo valor de la lista ordenada (vehiculo más/menos caro)
     const caro = copia.shift();
     const barato = copia.pop();
-    console.log(`Vehículo más caro: ${caro.Marca} ${caro.Modelo}`);
-    console.log(`Vehículo más barato: ${barato.Marca} ${barato.Modelo}`);
-    console.log(`Vehículo que contiene en el modelo la letra ‘Y’: ${encontrado.Marca} ${encontrado.Modelo} ${encontrado.Precio.toLocaleString('en-EN', {style: "currency", currency: "USD", minimumFractionDigits: 2})}`);
+    console.log(`Vehículo más caro: ${caro.marca} ${caro.modelo}`);
+    console.log(`Vehículo más barato: ${barato.marca} ${barato.modelo}`);
+    console.log(`Vehículo que contiene en el modelo la letra ‘Y’: ${encontrado.marca} ${encontrado.modelo} ${encontrado.precio_formateado()}`);
 };
 
 imprimirOrdenados = () => {
     // Crea copia del listado de vehiculos para modificar
     const copia = [...datos];
     // Ordena la lista copiada de mayor a menor precio
-    copia.sort((a,b) => b.Precio - a.Precio);
+    copia.sort((a,b) => b.precio - a.precio);
     console.log("Vehículos ordenados por precio de mayor a menor:");
     copia.forEach(vehiculo => {
-        console.log(`${vehiculo.Marca} ${vehiculo.Modelo}`);
+        console.log(`${vehiculo.marca} ${vehiculo.modelo}`);
     });
 };
 
